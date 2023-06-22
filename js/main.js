d5ff73e749311eb6da5f95bd6a9afc09d9cd3cbc
abramowsergey1992 <71379145+abramowsergey1992@users.noreply.github.com>
2023-06-22 18:02:45 +0500

    fix

--- js/main.js
+++ js/main.js
@@ -252,43 +252,6 @@ $(function () {
 	}
 });
 
-function header() {
-	let header = $(".header");
-	let prevscroll = $(window).scrollTop();
-	// if (isFront) {
-	// 	prevscroll = window.innerHeight * 2;
-	// }
-
-	if (prevscroll > 5) {
-		header.addClass("_bg");
-	} else {
-		header.removeClass("_bg");
-	}
-	$(window).scroll(() => {
-		let currentScroll = $(window).scrollTop();
-
-		if (currentScroll > 5) {
-			header.addClass("_bg");
-		} else {
-			header.removeClass("_bg");
-		}
-		if (currentScroll > prevscroll) {
-			header.addClass("_header-hidden");
-		} else {
-			header.removeClass("_header-hidden");
-		}
-		if (currentScroll <= 10) {
-			header.removeClass("_header-hidden");
-		}
-		prevscroll = currentScroll;
-	});
-}
-$(function () {
-	header();
-});
-
-$(function(){})
-$(function(){})
 $(function () {
 	let offset = window.innerWidth < 600 ? 60 : 120;
 	AOS.init({
@@ -325,6 +288,43 @@ $(function () {
 			}, $(detail).data("speed"));
 		}
 	});
+});
+
+$(function(){})
+$(function(){})
+function header() {
+	let header = $(".header");
+	let prevscroll = $(window).scrollTop();
+	// if (isFront) {
+	// 	prevscroll = window.innerHeight * 2;
+	// }
+
+	if (prevscroll > 5) {
+		header.addClass("_bg");
+	} else {
+		header.removeClass("_bg");
+	}
+	$(window).scroll(() => {
+		let currentScroll = $(window).scrollTop();
+
+		if (currentScroll > 5) {
+			header.addClass("_bg");
+		} else {
+			header.removeClass("_bg");
+		}
+		if (currentScroll > prevscroll) {
+			header.addClass("_header-hidden");
+		} else {
+			header.removeClass("_header-hidden");
+		}
+		if (currentScroll <= 10) {
+			header.removeClass("_header-hidden");
+		}
+		prevscroll = currentScroll;
+	});
+}
+$(function () {
+	header();
 });
 
 $(function(){})
