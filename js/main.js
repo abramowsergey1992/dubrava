8225ecdcd75a34b00049ee648620fa13e35191b8
abramowsergey1992 <71379145+abramowsergey1992@users.noreply.github.com>
2023-06-28 14:24:11 +0500

    fix

--- js/main.js
+++ js/main.js
@@ -272,6 +272,59 @@ $(function () {
 	}
 });
 
+$(function () {
+	var vh = window.innerHeight * 0.01;
+	document.documentElement.style.setProperty("--vh", `${vh}px`);
+	var width = window.innerWidth;
+	window.addEventListener("resize", () => {
+		if (width != window.innerWidth) {
+			var vh = window.innerHeight * 0.01;
+			document.documentElement.style.setProperty("--vh", `${vh}px`);
+			width = window.innerWidth;
+		}
+	});
+	let offset = window.innerWidth < 600 ? 60 : 120;
+	AOS.init({
+		// Global settings:
+		disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+		startEvent: "DOMContentLoaded", // name of the event dispatched on the document, that AOS should initialize on
+		initClassName: "aos-init", // class applied after initialization
+		animatedClassName: "aos-animate", // class applied on animation
+		useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+		disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+		debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+		throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+		// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+		offset: offset, // offset (in px) from the original trigger point
+		delay: 0, // values from 0 to 3000, with step 50ms
+		duration: 800, // values from 0 to 3000, with step 50ms
+		easing: "ease", // default easing for AOS animations
+		once: true, // whether animation should happen only once - while scrolling down
+		mirror: false, // whether elements should animate out while scrolling past them
+		anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
+	});
+	document.addEventListener("aos:in", ({ detail }) => {
+		console.log(detail.hasAttribute("data-speed"), detail);
+		if (detail.hasAttribute("data-count")) {
+			let count = $(detail).data("count");
+			$(detail).css({
+				width: $(detail).width(),
+				height: $(detail).height(),
+			});
+			let i = 0;
+			let interval = setInterval(function () {
+				i++;
+				$(detail).text(i);
+				if (i >= count) {
+					$(detail).removeAttr("style");
+					clearInterval(interval);
+				}
+			}, $(detail).data("speed"));
+		}
+	});
+});
+
 $(function(){})
 function header() {
 	let header = $(".header");
@@ -321,49 +374,6 @@ function header() {
 }
 $(function () {
 	header();
-});
-
-$(function () {
-	let offset = window.innerWidth < 600 ? 60 : 120;
-	AOS.init({
-		// Global settings:
-		disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-		startEvent: "DOMContentLoaded", // name of the event dispatched on the document, that AOS should initialize on
-		initClassName: "aos-init", // class applied after initialization
-		animatedClassName: "aos-animate", // class applied on animation
-		useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-		disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-		debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-		throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-
-		// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-		offset: offset, // offset (in px) from the original trigger point
-		delay: 0, // values from 0 to 3000, with step 50ms
-		duration: 800, // values from 0 to 3000, with step 50ms
-		easing: "ease", // default easing for AOS animations
-		once: true, // whether animation should happen only once - while scrolling down
-		mirror: false, // whether elements should animate out while scrolling past them
-		anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
-	});
-	document.addEventListener("aos:in", ({ detail }) => {
-		console.log(detail.hasAttribute("data-speed"), detail);
-		if (detail.hasAttribute("data-count")) {
-			let count = $(detail).data("count");
-			$(detail).css({
-				width: $(detail).width(),
-				height: $(detail).height(),
-			});
-			let i = 0;
-			let interval = setInterval(function () {
-				i++;
-				$(detail).text(i);
-				if (i >= count) {
-					$(detail).removeAttr("style");
-					clearInterval(interval);
-				}
-			}, $(detail).data("speed"));
-		}
-	});
 });
 
 $(function(){})
